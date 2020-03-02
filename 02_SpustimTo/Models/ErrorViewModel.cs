56c54c756b4ad75dd3238b91771f332d49a10ed8
Jakub Šenkýř <senkyr@spstrutnov.cz>
2020-03-02 13:58:14 +0100

    Nový projekt "Spustím to?"

---
+++ 02_SpustimTo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _02_SpustimTo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
