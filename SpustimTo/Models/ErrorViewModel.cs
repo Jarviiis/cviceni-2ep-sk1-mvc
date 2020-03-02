9060582edf954af6d0dfc942c12c97f3b8ae61be
Jakub Šenkýř <senkyr@spstrutnov.cz>
2020-03-02 14:08:01 +0100

    Přizpůsobení defaultních stránek

---
+++ SpustimTo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SpustimTo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
